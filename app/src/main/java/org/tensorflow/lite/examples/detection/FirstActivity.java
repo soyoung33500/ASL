befaa5b871535955a8659b3286bb00df5e01501e
soyoung33500 <soyoung33500@gmail.com>
2022-10-28 09:14:44 +0900

    Initial commit

---
+++ app/src/main/java/org/tensorflow/lite/examples/detection/FirstActivity.java
@@ -0,0 +1,59 @@
+package org.tensorflow.lite.examples.detection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ConfigurationInfo;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import org.tensorflow.lite.examples.detection.customview.OverlayView;
+import org.tensorflow.lite.examples.detection.env.ImageUtils;
+import org.tensorflow.lite.examples.detection.env.Logger;
+import org.tensorflow.lite.examples.detection.env.Utils;
+import org.tensorflow.lite.examples.detection.tflite.Classifier;
+import org.tensorflow.lite.examples.detection.tflite.YoloV5Classifier;
+import org.tensorflow.lite.examples.detection.tracking.MultiBoxTracker;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+import android.view.View;
+
+// GIF add library +
+import com.bumptech.glide.Glide;
+
+
+
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+
+//        Glide.with(this).load(R.raw.img).override(200, 200).into(ivLoading);
+
+        Button imageButton = (Button) findViewById(R.id.btn1);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), DetectorActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
